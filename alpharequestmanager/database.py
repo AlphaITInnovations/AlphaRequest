a17f6e74ddceefd51d899d96564215dd224b97eb
Marco Schneider <Marco.Schneider@alpha-it-innovations.org>
2025-10-10 15:13:04 +0200

    env update

--- alpharequestmanager/database.py
+++ alpharequestmanager/database.py
@@ -8,11 +8,9 @@ from typing import List, Optional
 from .models import Ticket, RequestStatus
 from .logger import logger
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "tickets.db")
 
 def get_connection():
-    conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect("data/tickets.db")
     conn.row_factory = sqlite3.Row
     return conn
 
--- main.py
+++ main.py
@@ -12,14 +12,13 @@ if __name__ == "__main__":
 
     if sys.platform.startswith("win"):
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    print(config.as_dict())
 
     uvicorn.run(
         app,
         host="0.0.0.0",
         port=5000,
         reload=False,
-        ssl_keyfile="cert/key.pem",
-        ssl_certfile="cert/cert.pem"
+        ssl_keyfile="data/cert/key.pem",
+        ssl_certfile="data/cert/cert.pem"
     )
 
